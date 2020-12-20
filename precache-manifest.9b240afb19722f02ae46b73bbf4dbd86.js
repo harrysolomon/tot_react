b8ee9ef4aca64905f36478ec045368fe8a612666
hsolomon602 <harrysolomon1993@gmail.com>
2020-12-20 10:20:34 -0800

    Updates

---
+++ precache-manifest.9b240afb19722f02ae46b73bbf4dbd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d36943d84c601896ad0180c9e00ebc8",
+    "url": "/tot_react/index.html"
+  },
+  {
+    "revision": "f351d3d06f21d2d4eca7",
+    "url": "/tot_react/static/css/2.ab369834.chunk.css"
+  },
+  {
+    "revision": "f351d3d06f21d2d4eca7",
+    "url": "/tot_react/static/js/2.6a130e13.chunk.js"
+  },
+  {
+    "revision": "cd1607c6eb998271903b61a55274a8c1",
+    "url": "/tot_react/static/js/2.6a130e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ca529664589b20fbf2",
+    "url": "/tot_react/static/js/main.dbac6c5b.chunk.js"
+  },
+  {
+    "revision": "8a598212218a73d9af4c",
+    "url": "/tot_react/static/js/runtime-main.d0c707b9.js"
+  }
+]);
