8edd4307c3e205d7d0ebf480451fb27439a61cae
hsolomon602 <harrysolomon1993@gmail.com>
2021-03-01 18:42:01 -0800

    Moved around some api functions, fixed some things with create workflow and cannot figure out how to add more objects to a get time saver id request

--- functions/time_saved.js
+++ functions/time_saved.js
@@ -1,34 +1,12 @@
 var schemas = require('../functions/schemas')
 module.exports = {
-    current_cost: function(req){
-        let date_dict = schemas.date_dictionary()
-        let interval = "quarter"
-        let time_now_period = "hour"
-        let forecast_length = 8
-        let total_cur_employee_costs = 0
-        
-        for (let i = 0;i < req.length;++i){
-            
-            let cur_employee_cost = req[i].employees.cost * req[i].current_time_spent * date_dict[time_now_period][req[i].employees.period]
-
-            total_cur_employee_costs += cur_employee_cost
-
-        }
-        
-        let final_cur_employee_cost_array = []
-
-        for (let i = 0;i < forecast_length;++i) {
-            final_cur_employee_cost_array.push(total_cur_employee_costs)
-        }
-        return final_cur_employee_cost_array
-    },
-
     new_cost: function(req){
         let date_dict = schemas.date_dictionary()
         let interval = "quarter"
         let time_now_period = "hour"
         let forecast_length = 8
         let total_new_employee_costs = 0
+        let total_cur_employee_costs = 0
 
         for (let i = 0;i < req.length;++i){
             
@@ -37,19 +15,40 @@ module.exports = {
             } else {
                 time_save_convert = req[i].products.time_save * date_dict[req[i].products.time_unit][time_now_period]
             }
-            
+
+            let cur_employee_cost = req[i].employees.cost * req[i].current_time_spent * date_dict[time_now_period][req[i].employees.period]
             let product_cost_rate = req[i].products.cost * date_dict[interval][req[i].products.period]
             let new_employee_cost = (req[i].employees.cost * (req[i].current_time_spent - time_save_convert) * date_dict[time_now_period][req[i].employees.period]) + product_cost_rate
-
+            
             total_new_employee_costs += new_employee_cost
+            total_cur_employee_costs += cur_employee_cost
+            
 
         }
         
         let final_new_employee_cost_array = []
+        let final_cur_employee_cost_array = []
 
         for (let i = 0;i < forecast_length;++i) {
             final_new_employee_cost_array.push(total_new_employee_costs)
+            final_cur_employee_cost_array.push(total_cur_employee_costs)
         }
-        return final_new_employee_cost_array
+        //console.log(final_new_employee_cost_array)
+
+        let result = schemas.line_chart_schema()
+
+        final_cur_employee_cost_array.reduce( (prev, curr,i) =>  result["data"]["datasets"][0]["data"][i] = Math.round(prev + curr), 0)
+        final_new_employee_cost_array.reduce( (prev, curr,i) =>  result["data"]["datasets"][1]["data"][i] = Math.round(prev + curr), 0)
+
+        let combined_cost = result["data"]["datasets"][0]["data"].concat(result["data"]["datasets"][1]["data"]);
+
+        let minimum_val = Math.min.apply(Math, combined_cost);
+        let maximum_val = Math.max.apply(Math, combined_cost);
+
+        result["options"]["scales"]["yAxes"][0]["ticks"]["suggestedMin"] = minimum_val
+        result["options"]["scales"]["yAxes"][0]["ticks"]["suggestedMax"] = maximum_val
+
+
+        return result
     }
 }
